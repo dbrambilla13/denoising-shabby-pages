1e03df5ea90b62de81866c531b7fe664688b157a
dbrambilla13 <daniele.bram@gmail.com>
2024-03-06 21:25:01 +0100

    update and add reqs

--- website.py
+++ website.py
@@ -7,7 +7,7 @@ import torchvision.transforms.functional as F
 
 
 # model_name = 'mild-snowflake-30-epoch=157-mse_val=0.018.ckpt'
-model_name = 'pretty-elevator-27-epoch=236-mse_val=0.017.ckpt'
+model_name = "pretty-elevator-27-epoch=236-mse_val=0.017.ckpt"
 
 trained_model = DenoisingNet.load_from_checkpoint(model_name).eval().cpu()
 
@@ -15,22 +15,21 @@ st.title("Denoising Shabby Pages")
 
 # split='train'
 # split='validate'
-split='test'
+split = "test"
 
 
-input_folder = os.path.join(split, split, f'{split}_shabby')
-target_folder = os.path.join(split, split, f'{split}_cleaned')
+input_folder = os.path.join(split, split, f"{split}_shabby")
+target_folder = os.path.join(split, split, f"{split}_cleaned")
 
-st.header('Input Selection')
-img = st.selectbox("input image",options=os.listdir(input_folder))
+st.header("Input Selection")
+img = st.selectbox("input image", options=os.listdir(input_folder))
 
 
 input_imgpath = os.path.join(input_folder, img)
 target_imgpath = os.path.join(target_folder, img)
 
 
-
-x = torchvision.io.read_image(input_imgpath).to(dtype=torch.float32)/255.0
+x = torchvision.io.read_image(input_imgpath).to(dtype=torch.float32) / 255.0
 
 
 image = F.to_pil_image(x.squeeze(0), mode="L")
@@ -38,16 +37,18 @@ image = F.to_pil_image(x.squeeze(0), mode="L")
 
 st.image(image)
 
-st.header('Denoising Model Output')
+st.header("Denoising Model Output")
 
 y_hat = trained_model(x.unsqueeze(0))
 
 col1, col2 = st.columns(2)
 with col1:
-
+    st.subheader("Model Prediction")
     st.image(F.to_pil_image(y_hat.squeeze(0), mode="L"))
 
 with col2:
-    y = torchvision.io.read_image(target_imgpath).to(dtype=torch.float32)/255.0
+
+    st.subheader("Target")
+    y = torchvision.io.read_image(target_imgpath).to(dtype=torch.float32) / 255.0
 
     st.image(F.to_pil_image(y.squeeze(0), mode="L"))
